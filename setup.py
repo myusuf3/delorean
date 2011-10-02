7fd531f710a27aa5d5ace33a7116522b2da6e653
myusuf3 <yusuf.mahdi@gmail.com>
2011-10-02 18:57:05 -0400

    adding more setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from distutils.core import setup
 setup(
     name='Delorean',
     version='0.1dev',
-    packages=['delorean',],
+    url='https://github.com/myusuf3/delorean'
+    author="Mahdi Yusuf",
+    author_email="yusuf.mahdi@gmail.com",
+    packages=[
+    'delorean',
+    ],
+
     license='MIT license',
     long_description=open('README.rst').read(),
 )
