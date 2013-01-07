05ac0e43f4a5c5fdf61472ea82e7aa71a71d2812
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2013-01-07 01:17:33 -0500

    committed ground work to stops api

--- delorean/utils.py
+++ delorean/utils.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+from pytz import timezone
 from dateutil.parser import parse
 from dateutil.rrule import rrule
-from .data import Delorean
+from .exceptions import DeloreanInvalidDatetime
+
+from .data import Delorean, is_datetime_naive
 
 UTC = "UTC"
 
@@ -20,13 +24,38 @@ def capture(s, dayfirst=True, timezone=UTC):
     return do
 
 
-def range(freq, start, stop, interval):
+def stops(freq, interval=1, count=None, wkst=None, bysetpos=None,
+          bymonth=None, bymonthday=None, byyearday=None, byeaster=None,
+          byweekno=None, byweekday=None, byhour=None, byminute=None,
+          bysecond=None, timezone=UTC, start=None, until=None):
     """
     This will create a list of delorean objects the apply to
     setting possed in.
     """
-    pass
+    tz = timezone(timezone)
+    # check to see if datetimees passed in are naive if so process them
+    # with given timezone.
+    if is_datetime_naive(start) and is_datetime_naive(until):
+        # start = tz.localize(start)
+        # until = tz.localize(until)
+        pass
+    else:
+        raise DeloreanInvalidDatetime('Provide a naive datetime object')
 
+    # if no datetimes are passed in create a proper datetime object for
+    # start default because default in dateutil is datetime.now() :(
+    if start is None:
+        start = datetime.utcnow()
+
+    for dt in rrule(freq, interval=1, count=count, wkst=None, bysetpos=None,
+          bymonth=None, bymonthday=None, byyearday=None, byeaster=None,
+          byweekno=None, byweekday=None, byhour=None, byminute=None,
+          bysecond=None, until=until, dtstart=start):
+        # make the delorean object
+        # yield it.
+
+        d = Delorean(datetime=dt, timezone=timezone)
+        yield d
 
 def utcnow():
     """
