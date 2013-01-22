42263f7fdf86378e8c0889a37c392ea12eb8820c
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2013-01-22 00:46:53 -0500

    cleaning up init

--- delorean/__init__.py
+++ delorean/__init__.py
@@ -1,10 +1,16 @@
 __version__ = (0, 1, 0)
 
-try:
-    from .utils import *
-    from .data import Delorean, datetime_timezone, localize, normalize
-    from dateutil.rrule import (YEARLY,
-        MONTHLY, WEEKLY, DAILY, HOURLY,
-        MINUTELY, SECONDLY, MO, TU, WE, TH, FR, SA, SU)
-except:
-    pass
+from dateutil.rrule import (
+                            YEARLY, MONTHLY, WEEKLY, DAILY, HOURLY,
+                            MINUTELY, SECONDLY, MO, TU, WE, TH, FR,
+                            SA, SU
+)
+
+from .interface import parse, stops, epoch, flux, utcnow, now
+from .exceptions import DeloreanInvalidTimezone, DeloreanInvalidDatetime
+from .dates import (
+                   move_datetime_day, move_datetime_week,
+                   move_datetime_month, move_datetime_year,
+                   move_datetime_namedday, Delorean, datetime_timezone,
+                   localize, normalize
+)
