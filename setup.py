2d6c4fdb55d0aa8d1e71077cb03a416b0f9a1076
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2013-02-25 23:17:43 -0500

    supporting up to python3.2

--- setup.py
+++ setup.py
@@ -31,5 +31,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
     ),
 )
