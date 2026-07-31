3c4fcd11e0544098f827aa2a614bbf4e999f9c0c
Mahdi Yusuf <yusuf.mahdi@gmail.com>
2026-07-31 15:22:04 -0400

    docs: update license years and include LICENSE.txt in docs

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'delorean'
-copyright = u'2013, Mahdi Yusuf'
+copyright = u'2012-2026, Mahdi Yusuf'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
